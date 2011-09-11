85d690f5228cd24a10c2c511309e0d7117ba906d
imilh <imilh>
2011-09-11 08:41:20 +0000

    we don't use regex for this

--- mkpkgindb.sh
+++ mkpkgindb.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-# $Id: mkpkgindb.sh,v 1.1 2011/03/03 14:43:12 imilh Exp $
+# $Id: mkpkgindb.sh,v 1.2 2011/09/11 08:41:20 imilh Exp $
 
 echo "/* automatically generated, DO NOT EDIT */"
 echo '#define CREATE_DRYDB " \'
-${SEDCMD} -E -e 's/$/ \\/' -e 's/\"/\\\"/g' pkgin.sql
+${SEDCMD} -e 's/$/ \\/' -e 's/\"/\\\"/g' pkgin.sql
 echo '"' 
