33ebbd1bd6d925f39bc9a2d906b0c4788222976b
imilh <imilh>
2011-10-15 22:24:51 +0000

    oops, strip off file://

--- actions.c
+++ actions.c
@@ -1,4 +1,4 @@
-/* $Id: actions.c,v 1.33 2011/10/15 21:05:27 imilh Exp $ */
+/* $Id: actions.c,v 1.34 2011/10/15 22:24:51 imilh Exp $ */
 
 /*
  * Copyright (c) 2009, 2010, 2011 The NetBSD Foundation, Inc.
@@ -118,7 +118,7 @@ pkg_download(Plisthead *installhead)
 
 		/* if pkg's repo URL is file://, just symlink */
 		if (strncmp(pkg_url, SCHEME_FILE, strlen(SCHEME_FILE)) == 0) {
-			if (symlink(pkg_url, pkg_fs) < 0)
+			if (symlink(&pkg_url[strlen(SCHEME_FILE) + 3], pkg_fs) < 0)
 				errx(EXIT_FAILURE, MSG_SYMLINK_FAILED, pkg_fs);
 			printf(MSG_SYMLINKING_PKG, pkg_url);
 			continue;
