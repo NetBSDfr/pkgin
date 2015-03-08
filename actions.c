9ae23f0f08685a2fe7a08cff1ca52533489ea3cf
Emile 'iMil' Heitor <imil@NetBSD.org>
2015-03-08 18:14:24 +0100

    a wild typo appears, forgot dash before ffu

--- actions.c
+++ actions.c
@@ -314,7 +314,7 @@ do_pkg_install(Plisthead *installhead)
 		if (pinstall->old != NULL && access(preserve, F_OK) != -1)
 			/* set temporary force flags */
 			/* append verbosity if requested */
-			pflags = verb_flag("ffu");
+			pflags = verb_flag("-ffu");
 		else
 			/* every other package */
 			pflags = verb_flag("-D");
