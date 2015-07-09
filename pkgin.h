1f9e65fff1c98b8a5c8c4a851d339325f7c589d5
Jonathan Perkin <jperkin@joyent.com>
2015-07-09 15:49:21 +0100

    Only display parsable list output when -p is used.
    Previously list output would differ depending on whether the output was
    a tty or not, causing issues with various automated tools.

--- pkgin.h
+++ pkgin.h
@@ -212,6 +212,7 @@ extern uint8_t		verbosity;
 extern uint8_t		package_version;
 extern uint8_t		pi_upgrade; /* pkg_install upgrade */
 extern uint8_t		parsable;
+extern uint8_t		pflag;
 extern int		r_plistcounter;
 extern int		l_plistcounter;
 extern char		*env_repos;
