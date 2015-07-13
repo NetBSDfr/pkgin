2856b3a6fdc21390744991835351c1e1e6f6541b
Jonathan Perkin <jperkin@joyent.com>
2015-07-13 17:06:27 +0100

    Stream package fetches directly to the local file.
    Previously package downloads would go through download_file() and cache
    the entire file in RAM before writing to disk.  Introduce a new
    download_pkg() function which streams 4K at a time and significantly
    reduces memory usage.

--- pkgin.h
+++ pkgin.h
@@ -227,6 +227,7 @@ extern Preflist		**preflist;
 
 /* download.c*/
 Dlfile		*download_file(char *, time_t *);
+ssize_t		download_pkg(char *, FILE *);
 /* summary.c */
 int		update_db(int, char **);
 void		split_repos(void);
