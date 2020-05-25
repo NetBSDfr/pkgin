50a9f57f1243ee198c10f10c1f5669a519a45117
Michael Forney <mforney@mforney.org>
2020-05-24 23:11:57 -0700

    Set fetchTimeout in main to fix gcc 10 build error
    gcc 10 enables -fno-common by default, so duplicate definitions of
    globals result in build errors.
    
    To fix this, change fetchTimeout in main rather than making a
    separate definition.

--- main.c
+++ main.c
@@ -55,6 +55,9 @@ main(int argc, char *argv[])
 	/* Default to not doing \r printouts if we don't send to a tty */
 	parsable = !isatty(fileno(stdout));
 
+	/* Wait 15 seconds before timeout */
+	fetchTimeout = 15;
+
 	while ((ch = getopt(argc, argv, "dhyfFPvVl:nc:t:p")) != -1) {
 		switch (ch) {
 		case 'f':
